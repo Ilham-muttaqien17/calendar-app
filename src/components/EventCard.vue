419a39db1ce2e8323cba6bdaaeb90da1fef91272
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2023-07-07 11:14:17 +0700

    Add custom max width
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col bg-color text-white text-sm max-w-[150px] p-2 overflow-hidden rounded-md"
+    class="flex flex-col bg-color cmax-width text-white text-sm w-full p-2 overflow-hidden rounded-md"
     :style="customStyle"
   >
     <span>{{ item.name }}</span>
@@ -17,11 +17,16 @@ export default {
       type: Object,
       default: () => {},
     },
+    maxWidth: {
+      type: Number,
+      default: 150,
+    },
   },
   computed: {
     customStyle() {
       return {
         "--bg-color": this.item.color,
+        "--max-width": this.maxWidth + "px",
       };
     },
   },
@@ -31,5 +36,8 @@ export default {
 <style scoped>
 .bg-color {
   background-color: var(--bg-color);
+}
+.cmax-width {
+  max-width: var(--max-width);
 }
 </style>
